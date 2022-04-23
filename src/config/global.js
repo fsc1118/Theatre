d5f740475eb09bc8ac95e7c889aeaaff1d8a816f
Min Lu <70182873+minlu21@users.noreply.github.com>
2022-04-23 15:53:42 -0400

    changed proxy

--- src/config/global.js
+++ src/config/global.js
@@ -9,7 +9,7 @@
 // for local development, change SERVER_IP to "https://localhost:8080/"
 // for deployment to heroku, change SERVER_IP to https://cs348-theatre.herokuapp.com/"
 let AppConfig = {
-    SERVER_IP: "https://localhost:8080/",
+    SERVER_IP: "https://cs348-theatre.herokuapp.com/",
     CONNECTION_RETRY: 5,
     NETWORK_ERROR_MESSAGE: () => {
         window.location.replace("Error.html")
