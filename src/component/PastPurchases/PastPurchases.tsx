79ac182637b81861f3b11badb8bf1cc7286d1ffb
Peter Zong <peterzong0525@gmail.com>
2022-04-30 05:09:15 -0400

    PurchaseComplete page when finished with order

---
+++ src/component/PastPurchases/PastPurchases.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import {Container} from "react-bootstrap";
+
+export let PastPurchases = (props: any) => {
+    return(        
+    <Container>
+    Past purchases
+    </Container>)
+}
---
+++ src/component/PurchaseComplete/PurchaseComplete.tsx
@@ -0,0 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { Container, Button } from "react-bootstrap";
+import React, { useState, useEffect} from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+
+export let PurchaseComplete = (props: any) => {
+    type PurchaseInfo = {
+        movie_name?: string,
+        total_paid?: string
+    }
+
+    const {state} = useLocation() as { state: PurchaseInfo }
+    const [purchaseInfo, setPurchaseInfo] = useState<PurchaseInfo>({})
+
+    useEffect(() => {
+        setPurchaseInfo(state)
+        return () => {}
+    }, [state])
+
+    const navigate = useNavigate()
+
+    function goBackHome() {
+        navigate('/')
+    }
+
+    return (
+        <>
+        { purchaseInfo != {} ?
+        <Container>
+            <br></br>
+            <br></br>
+            <h1 className="text-center">Order Complete. Your Ticket Has Been Purchased!</h1>
+            <br></br>
+            <h3 className="text-center">Movie: {purchaseInfo.movie_name}</h3>
+            <h3 className="text-center">Total Paid: {purchaseInfo.total_paid}</h3>
+            <Button 
+                onClick={(e: any) => goBackHome()}>
+                Home
+            </Button>
+        </Container> :
+        undefined}
+        </>
+    )
+}
